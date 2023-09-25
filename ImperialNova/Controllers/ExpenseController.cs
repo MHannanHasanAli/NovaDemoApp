31f1db93ee4c0dfa8d7994882635dfbdab5f947e
Muhammad Hannan Hasan Ali <muhammadhannanhasanali@outlook.com>
2023-09-25 15:17:09 +0500

    done

--- ImperialNova/Controllers/ExpenseController.cs
+++ ImperialNova/Controllers/ExpenseController.cs
@@ -14,6 +14,7 @@ using System.Web.Mvc;
 
 namespace ImperialNova.Controllers
 {
+    [Authorize]
     public class ExpenseController : Controller
     {
         public ActionResult Index(string SearchTerm = "")
